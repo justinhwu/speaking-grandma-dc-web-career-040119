50d969f65b84df8dc111f94def9994919af4f853
Justin Wu <justinhwu95@gmail.com>
2019-03-15 01:06:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,6 @@ def speak_to_grandma(phrase)
     return "i love you too pumpkin!".upcase
   elsif (phrase == phrase.upcase)
     return "no, not since 1938!".upcase
-  
   else 
     return "huh?! speak up, sonny!".upcase
   end 
